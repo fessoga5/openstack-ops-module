057098b6af9876d9eb2d2c68274356eff13fdcfe
fessoga5 <fessoga5@gmail.com>
2015-11-23 23:31:25 +0800

    create instance code

---
+++ admin_openrc.sh
@@ -0,0 +1,7 @@
+export LC_ALL="en_US.UTF-8"
+export OS_TOKEN=e4ae697027de42e433d5
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=12345
+export OS_URL=http://controller:35357/v2.0
+export OS_AUTH_URL=http://controller:35357/v2.0
--- src/create_instance.py
+++ src/create_instance.py
@@ -1,8 +1,47 @@
 #!/usr/bin/env python2.7
-from openstack import connection
-conn = connection.Connection(auth_url="http://openstack:5000/v3",
-                             project_name="big_project",
-                             username="SDK_user",
-                             password="Super5ecretPassw0rd")
-for container in conn.object_store.containers():
-   print(container.name)
+# vim: sts=4 ts=4 sw=4 et ai
+import os
+import time
+#from credentials import get_nova_credentials_v2
+from novaclient.client import Client
+
+def get_config_yaml( path_config=os.path.dirname(os.path.realpath(__file__)) + "/../config.d/config.yaml", name_config="openstack"):
+    import yaml
+    from yaml import Loader, SafeLoader
+
+    def construct_yaml_str(self, node):
+        # Override the default string handling function 
+        # to always return unicode objects
+        return self.construct_scalar(node)
+
+    Loader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
+    SafeLoader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
+
+    f = open(path_config)
+    # use safe_load instead load
+    dataMap = yaml.load(f)[name_config]
+    f.close()
+    return dataMap
+
+def get_nova_credentials_v2():
+    d = get_config_yaml()
+    return d
+
+try:
+    instance_name = "test"
+    get_cnf = get_config_yaml(name_config="csserver");
+    credentials = get_nova_credentials_v2()
+    nova_client = Client(**credentials)
+
+    image = nova_client.images.find(name=get_cnf["image_name"])
+    flavor = nova_client.flavors.find(name=get_cnf["image_type"])
+    net = nova_client.networks.find(label=get_cnf["network"])
+    nics = [{'net-id': net.id}]
+    instance = nova_client.servers.create(name=instance_name, image=image,
+                                      flavor=flavor, key_name=None, nics=nics)
+    print("Sleeping for 5s after create command")
+    time.sleep(5)
+    #print("List of VMs")
+    #print(nova_client.servers.list())
+finally:
+    print("Execution Completed")
